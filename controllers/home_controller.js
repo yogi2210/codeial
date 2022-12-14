62cdd67845aaf95f709fa5ccb187cddeed4f0d0c
Yogesh <yogi.2210@gmail.com>
2022-12-14 16:59:39 +0530

    create sign up

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function(req, res){
     
+    console.log(req.cookies)
+    
+    res.cookie('user_id', 44)
+    
     return res.render('home', {
         title: 'Home'
     })
