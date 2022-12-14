7562ed1dab144028dc2fe867c419f9dd8a7d8814
Yogesh <yogi.2210@gmail.com>
2022-12-14 15:53:16 +0530

    creatr a sign in and sign up view and controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,25 @@ module.exports.profile = function(req, res){
         title: "Users Profile"
     })
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req, res){
+    //todo later
+}
+
+// sign in and create a session for user
+module.exports.createSession = function(req, res){
+    // todo later
+}
