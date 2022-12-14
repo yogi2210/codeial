8aa35778eb110747f4f6365e00ad0fa0efddd297
Yogesh <yogi.2210@gmail.com>
2022-12-14 13:03:16 +0530

    add layout

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,6 +1,13 @@
 module.exports.phone = function(req, res){
-    res.end( '<h1>Contact me on 7060357307</h1> ')
+    // res.end( '<h1>Contact me on 7060357307</h1> ')
+    return res.render('contact/contact_phone', {
+        title: 'phone'
+    })
 }
 module.exports.email = function(req, res){
-    res.end( '<h1>Contact me on yogi.2210@gmail.com</h1>' )
+    // res.end( '<h1>Contact me on yogi.2210@gmail.com</h1>' )
+    return res.render('contact/contact_email', {
+        name: "yogesh's",
+        title: 'email'
+    })
 }
